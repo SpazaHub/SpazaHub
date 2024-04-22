b1e29f29e1a5a105528e9ae5b7f76354665eceda
DeveshChiba <160410720+DeveshChiba@users.noreply.github.com>
2024-04-22 15:24:30 +0200

    Adding Admin js Devesh Chiba
    Devesh Chiba

---
+++ admin.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword,  createUserWithEmailAndPassword, signOut} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import{getDatabase, ref, set, get, child} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDP7N-NJ_PbOs41BbX6AgLZrBWdyP-odJU",
+    authDomain: "espaza-login-final.firebaseapp.com",
+    projectId: "espaza-login-final",
+    storageBucket: "espaza-login-final.appspot.com",
+    messagingSenderId: "358577580383",
+    appId: "1:358577580383:web:af7778db3431aaa2c72eea"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const db = getDatabase(app);
+
+document.getElementById('admin-login-btn').addEventListener('click', function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("admin-email").value;
+    const password = document.getElementById("admin-password").value;
+
+    // Authenticate user with Firebase Authentication
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            const username = email.substring(0, email.indexOf('@'));
+
+            set(ref(db, 'Admins/' + username), {
+                username: username,
+                email: email,
+            }).then(() => {
+                console.log("User registered successfully.");
+                window.location.href = "admin-landing.html";
+            }).catch((error) => {
+                console.error("Error updating database:", error);
+                alert("Error updating database: " + error.message); 
+            });
+        })
+        .catch((error) => {
+            console.error("Authentication error:", error);
+            alert("Invalid Login Credentials, please try again!");
+        });
+});
+
+
+
+
+
