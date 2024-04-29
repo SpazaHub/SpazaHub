9149e11a71382cb98bc07e2409d3827647feb8e9
DeveshChiba <160410720+DeveshChiba@users.noreply.github.com>
2024-04-29 13:54:55 +0200

    Update customer-landing.js Devesh Chiba
    This goes hand in hand with the customer-landing.html change. More of the js code is stored in the html file

--- customer-landing.js
+++ customer-landing.js
@@ -1,18 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getAuth} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDP7N-NJ_PbOs41BbX6AgLZrBWdyP-odJU",
-    authDomain: "espaza-login-final.firebaseapp.com",
-    projectId: "espaza-login-final",
-    storageBucket: "espaza-login-final.appspot.com",
-    messagingSenderId: "358577580383",
-    appId: "1:358577580383:web:af7778db3431aaa2c72eea"
-  };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
+// Function to sign out
 const signOutBtn = document.getElementById('sign-out-btn');
 signOutBtn.addEventListener('click', () => {
     auth.signOut()
@@ -24,5 +13,3 @@ signOutBtn.addEventListener('click', () => {
             alert("An error occurred. Please try again later.");
         });
 });
-
-
