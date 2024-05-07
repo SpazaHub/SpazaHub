0fda9234371bd4e44926ee15705a907784682241
DeveshChiba <160410720+DeveshChiba@users.noreply.github.com>
2024-05-07 09:35:32 +0200

    Update customer-cart.js

--- customer-cart.js
+++ customer-cart.js
@@ -1,6 +1,6 @@
 // Import Firebase modules
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
+import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js';
 
 // Firebase configuration
 const firebaseConfig = {
